72049c589a530662111ec411b752d838ffc6464c
KenstateAllied <Kenstatellc@gmail.com>
2024-03-18 17:10:01 +0300

    f

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         <div className={styles.imgbox}>
             <div className={`${styles.center} overflow-hidden`}>
                 <p data-aos='fade-down' className={styles.text}>Software Development, Implementation & Deployment</p>
-                <button data-aos='fade-up' className={`btn custom_btn ${styles.btn}`}>Contacts us with all your digital needs</button>
+                <button data-aos='fade-up' className={`btn custom_btn ${styles.btn}`}>Contact us with all your digital needs</button>
             </div>
             
         </div>
--- src/components/Testimonial/fakeData.js
+++ src/components/Testimonial/fakeData.js
@@ -6,23 +6,23 @@ export const fakeData = [
     {
         id: 1,
         img: image1,
-        name: 'Howard Arlene',
-        profession: 'Finance Manager',
-        message: "Your company is truly upstanding and is behind its product 100%. It's the perfect solution for our business. It has really helped our business."
+        name: 'SAFARICOM KE',
+        profession: 'Product Manager',
+        message: "Your company helped us integrate Mpesa micro frontends seamlessly."
     },
     {
         id: 2,
         img: image2,
-        name: 'Miles Esther',
-        profession: 'Public Relations Specialist',
-        message: "You won't regret it. I was amazed at the quality of it. I am really satisfied with my it."
+        name: 'NAIROBI SECURITIES EXCHANGE',
+        profession: 'DIRECTOR',
+        message: "Integration of state of the art stock brokerage"
     },
     {
         id: 3,
         img: image3,
-        name: 'Pena Norma',
-        profession: 'Public Relations Specialist',
-        message: "It fits our needs perfectly. Man, this thing is getting better and better as I learn more about it. You've saved our business!"
+        name: 'NAIROBI CITY COUNTY',
+        profession: 'SOFTWARE DEVELOPMENT',
+        message: "Developed & Deployed mobile ticketing payment application  "
     },
     {
         id: 4,
